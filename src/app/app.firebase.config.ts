bec35967ed1c5381f04b714147f1da5636e74b58
David Pincheira <dpincheira92@gmail.com>
2020-08-05 17:27:09 -0300

    clean credentials

--- src/app/app.firebase.config.ts
+++ src/app/app.firebase.config.ts
@@ -1,17 +1,8 @@
 export var config = {
-  apiKey: "AIzaSyDwgmtPOa1vB0xNXjKVu5PNbk3iOTV6-nk",
-  authDomain: "chat-8568d.firebaseapp.com",
-  databaseURL: "https://chat-8568d.firebaseio.com",
-  projectId: "chat-8568d",
-  storageBucket: "chat-8568d.appspot.com",
-  messagingSenderId: "1048089127675"
+  apiKey: "YOURKEY",
+  authDomain: "YOURCHATDOMAIN.firebaseapp.com",
+  databaseURL: "URLDATABASE",
+  projectId: "chat-XXXX",
+  storageBucket: "chat-XXXXX.appspot.com",
+  messagingSenderId: "XXXXXXXXXXXX"
 };
-
-/*
-	apiKey: "AIzaSyANFTSKPrUNFiB7YRGw-3Y71L91Qleg2Pc",
-    authDomain: "whatsapp2-b84ff.firebaseapp.com",
-    databaseURL: "https://whatsapp2-b84ff.firebaseio.com",
-    projectId: "whatsapp2-b84ff",
-    storageBucket: "",
-    messagingSenderId: "956210328641"
-*/
